c2cbd249fcdee35032819ad7cb60bae75fd66dcb
ericsondalay <eicsondalay@gmail.com>
2025-06-08 10:41:32 +0800

    modified userDashboard BillList

--- client/src/components/BillsList.vue
+++ client/src/components/BillsList.vue
@@ -251,14 +251,15 @@ function printReceipt(bill) {
 
         <div class="line"></div>
 
-        <div>Subtotal:    PHP ${bill.subTotal.toFixed(2)}</div>
-        <div>VAT Sales:    PHP ${bill.vatSales.toFixed(2)}</div>
-        <div>VAT Amount:   PHP ${bill.vatAmount.toFixed(2)}</div>
-        <div>Discount(20%): PHP ${bill.discount.toFixed(2)}</div>
-        <div>Total Amount:        PHP ${bill.totalAmount.toFixed(2)}</div>
-        <div>Cash Tendered:         PHP ${bill.cash.toFixed(2)}</div>
-        <div>Change:       PHP ${bill.change.toFixed(2)}</div>
-         <div>Senior/PWD:   ${bill.seniorOrPWD ? 'Yes' : 'No'}</div>
+       <div class="row1"><span>Subtotal:</span><span>PHP ${bill.subTotal.toFixed(2)}</span></div>
+       <div class="row1"><span>VAT Sales:</span><span>PHP ${bill.vatSales.toFixed(2)}</span></div>
+       <div class="row1"><span>VAT Amount:</span><span>PHP ${bill.vatAmount.toFixed(2)}</span></div>
+       <div class="row1"><span>Discount (20%):</span><span>PHP ${bill.discount.toFixed(2)}</span></div>
+       <div class="row1"><span>Total Amount:</span><span>PHP ${bill.totalAmount.toFixed(2)}</span></div>
+       <div class="row1"><span>Cash Tendered:</span><span>PHP ${bill.cash.toFixed(2)}</span></div>
+       <div class="row1"><span>Change:</span><span>PHP ${bill.change.toFixed(2)}</span></div>
+       <div class="row1"><span>Senior/PWD:</span><span>${bill.seniorOrPWD ? 'Yes' : 'No'}</span></div>
+
 
         <div class="line"></div>
 
