b4a38879e83f4d0d6d19894888feee95cb123ce8
ericsondalay <eicsondalay@gmail.com>
2025-06-12 11:27:15 +0800

    modify token refresh

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -143,11 +143,18 @@ exports.login = async (req, res) => {
     const accessToken = createAccessToken(user);
     const refreshToken = createRefreshToken(user);
 
+    // res.cookie("refreshToken", refreshToken, {
+    //   httpOnly: true,
+    //   // secure: false,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: "Strict",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });
+
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      // secure: false,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
