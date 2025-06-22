a0a440f991606658a4f61997dd7e5f7b5f49a53d
ericsondalay <eicsondalay@gmail.com>
2025-06-22 15:33:35 +0800

    modified dashboard

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,19 @@ const expenseRoutes = require("./routes/expensesRoute");
 const app = express();
 dotenv.config();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:9000",
-      "https://razons-pos.onrender.com",
-      "https://razons.onrender.com",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "http://localhost:9000",
+    "https://razons-pos.onrender.com",
+    "https://razons.onrender.com",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
